0d167e89e2a7d4c22981556f0e5cbdbb7a5d1e7f
Ryo Nakamura <r7kamura@gmail.com>
2014-01-29 04:23:02 +0900

    bundler gem faraday-lazyable

---
+++ lib/faraday/lazyable.rb
@@ -0,0 +1,7 @@
+require "faraday/lazyable/version"
+
+module Faraday
+  module Lazyable
+    # Your code goes here...
+  end
+end
---
+++ lib/faraday/lazyable/version.rb
@@ -0,0 +1,5 @@
+module Faraday
+  module Lazyable
+    VERSION = "0.0.1"
+  end
+end
